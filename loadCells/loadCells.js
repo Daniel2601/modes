3f7f04212c8b17946c115663b1189292f8459a0b
Daniel2601 <36491075+Daniel2601@users.noreply.github.com>
2022-06-04 19:58:29 +0200

    Add files via upload

--- loadCells/loadCells.js
+++ loadCells/loadCells.js
@@ -8,18 +8,18 @@ function setup() {
   background(100);
   textSize(16);
   b1 = createButton('Suivant');
-  b1.position(150, 150);
+  b1.position(150, 20);
   b1.mousePressed(addN);
   b2 = createButton('Précédent');
-  b2.position(0, 150);
+  b2.position(0, 20);
   b2.mousePressed(subN);
 
   //imageMode(CENTER);
 }
 
 function draw() {
-    image(img, 50, 250);
-    text("img:"+n, 75, 250);
+    image(img, 50, 100);
+    text("img:"+n, 75, 100);
 }
 
 
@@ -31,6 +31,6 @@ function addN() {
 
 function subN() {
   n = n - 1;
-  //img = loadImage('export2/Stack0'+n+'.png');
+  img = loadImage('export2/Stack0'+n+'.png');
   background(255);
 }
